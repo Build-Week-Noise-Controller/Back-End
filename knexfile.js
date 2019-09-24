1c750399075ce22c2886b8df6b92e4a72b869f7d
Carla Marvin <carlamarvin78@gmail.com>
2019-09-24 16:13:58 -0400

    Users complete

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,16 @@ module.exports = {
       },
       seeds: { directory: './data/seeds' },
     },
+    production: {
+      client: 'sqlite3',
+      connection: { filename: './data/noiseControl.db3' },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+        tableName: 'dbmigrations',
+      },
+      seeds: { directory: './data/seeds' },
+    },
     testing: {
       client: 'sqlite3',
       connection: { filename: './data/test.db3' },
